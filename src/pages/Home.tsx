23ca1c29cd05a66db3c419bdbc0532af4ba0f87b
Roihan <hroihan6@gmail.com>
2022-11-20 20:47:15 +0700

    Added floating button & modal

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,49 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {  IonCol ,IonRow ,IonButtons ,IonButton, IonContent, IonFab, IonFabButton, IonHeader, IonIcon, IonModal, IonPage, IonTitle, IonToolbar, IonGrid } from '@ionic/react';
+import {add, closeOutline} from 'ionicons/icons'
+import { useRef, useState } from 'react';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const modal = useRef<HTMLIonModalElement>(null);
+
+  const closeModal = () => {
+    setIsOpenModal(false);
+  }
+
+  const openModal = () => {
+    setIsOpenModal(true);
+  }
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+        <IonToolbar color="primary">
+          <IonTitle>List Ruangan</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent fullscreen color="dark">
+        <IonFab slot="fixed" vertical="bottom" horizontal="end" onClick={openModal}>
+          <IonFabButton>
+            <IonIcon icon={add}></IonIcon>
+          </IonFabButton>
+        </IonFab>
+
+        <IonModal ref={modal} isOpen={isOpenModal}>
+          <IonHeader>
+            <IonToolbar color="primary">
+              <IonCol>
+                <IonTitle>Modal</IonTitle>  
+              </IonCol>
+              <IonButtons slot='end'>
+                <IonButton onClick={closeModal}>Cancel</IonButton>
+              </IonButtons>
+            </IonToolbar>
+          </IonHeader>
+
+          <IonContent color="dark">
+            
+          </IonContent>
+        </IonModal>
       </IonContent>
     </IonPage>
   );
